7e50f7b4d38cb06647eb8becd2154cdb006d2337
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-05-10 17:34:34 -0300

    Add create plan card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <h2 className="home">Home</h2>;
+  return (
+    <>
+      <div className="create-plan-card">
+        <div className="create-plan-content__container">
+          <h1 className="create-plan-card__title">Great coffee made simple.</h1>
+          <p className="create-plan-card__text">
+            Start your mornings with the world’s best coffees. Try our expertly
+            curated artisan coffees from our best roasters delivered directly to
+            your door, at your schedule.{" "}
+          </p>
+          <Link to="/create" className="create-plan-card__button">
+            Create your plan
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
