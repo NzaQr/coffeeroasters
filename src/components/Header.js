dd76fbd932b81415bdb37077f188398199d65269
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-05-09 21:18:13 -0300

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Logo from "../assets/logo.svg";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <div className="header-container">
+      <Link to="/">
+        <img className="logo" src={Logo} alt="myteam logo" />
+      </Link>
+      <div className="nav">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars close">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="item">
+              <Link to="/create">Create Your Plan</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="nav-item-container">
+        <Link className="nav-item" to="/">
+          HOME
+        </Link>
+        <Link className="nav-item" to="/about">
+          ABOUT US
+        </Link>
+        <Link className="nav-item" to="/contact">
+          CREATE YOUR PLAN
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <h2 className="home">Home</h2>;
 }
