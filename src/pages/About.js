43a26ea7c95b6c09c83a5e3947235e4d63c20c0d
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-05-14 14:06:44 -0300

    Add responsive styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,32 +11,35 @@ export default function About() {
   return (
     <>
       <div className="about-card">
-        <h1 className="about-title">About Us</h1>
-        <p className="about-text">
-          Coffeeroasters began its journey of exotic discovery in 1999,
-          highlighting stories of coffee from around the world. We have since
-          been dedicated to bring the perfect cup - from bean to brew - in every
-          shipment.
-        </p>
+        <div className="about-content-container">
+          <h1 className="about-title">About Us</h1>
+          <p className="about-text">
+            Coffeeroasters began its journey of exotic discovery in 1999,
+            highlighting stories of coffee from around the world. We have since
+            been dedicated to bring the perfect cup - from bean to brew - in
+            every shipment.
+          </p>
+        </div>
       </div>
-      <div className="barista-img-container" />
-      <div className="commitment-container">
-        <h1 className="commitment-title">Our commitment</h1>
-        <p className="commitment-text">
-          We’re built on a simple mission and a commitment to doing good along
-          the way. We want to make it easy for you to discover and brew the
-          world’s best coffee at home. It all starts at the source. To locate
-          the specific lots we want to purchase, we travel nearly 60 days a year
-          trying to understand the challenges and opportunities in each of these
-          places. We collaborate with exceptional coffee growers and empower a
-          global community of farmers through with well above fair-trade
-          benchmarks. We also offer training, support farm community
-          initiatives, and invest in coffee plant science. Curating only the
-          finest blends, we roast each lot to highlight tasting profiles
-          distinctive to their native growing region.
-        </p>
+      <div className="comitment">
+        <div className="barista-img-container" />
+        <div className="commitment-content-container">
+          <h1 className="commitment-title">Our commitment</h1>
+          <p className="commitment-text">
+            We’re built on a simple mission and a commitment to doing good along
+            the way. We want to make it easy for you to discover and brew the
+            world’s best coffee at home. It all starts at the source. To locate
+            the specific lots we want to purchase, we travel nearly 60 days a
+            year trying to understand the challenges and opportunities in each
+            of these places. We collaborate with exceptional coffee growers and
+            empower a global community of farmers through with well above
+            fair-trade benchmarks. We also offer training, support farm
+            community initiatives, and invest in coffee plant science. Curating
+            only the finest blends, we roast each lot to highlight tasting
+            profiles distinctive to their native growing region.
+          </p>
+        </div>
       </div>
-
       <div className="quality-img-container" />
       <div className="quality-card">
         <h1 className="quality-title">Uncompromising quality</h1>
@@ -50,7 +53,7 @@ export default function About() {
         </p>
       </div>
       <h1 className="headquarters-title">Our headquarters</h1>
-      {headquarters}
+      <div className="headquarters-component">{headquarters}</div>
     </>
   );
 }
